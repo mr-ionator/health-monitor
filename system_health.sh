521e3d30439dd13425fa1a9cc4212706c9f312f2
mr-ionator <siddh05nov@gmail.com>
2026-01-27 16:32:30 +0000

    initialized main functions

--- system_health.sh
+++ system_health.sh
@@ -1,9 +1,59 @@
-#!bin/bash
+#!/bin/bash
 
 ####################################
 # A health monitor for your system
 # Author: mr-ionator
 # Date: 27-01-2026
 ####################################
+
+
+#color codes for output
+
+
+RED="\e[31m"
+GREEN="\32m"
+YELLOW="\33m"
+NC="\0m"
+
+#functions
+
+
+check_cpu() {
+	echo "checking cpu usage...."
+}
+
+check_memory() {
+	echo "checking memory usage...."
+}
+
+check_disk() {
+	echo "cecking disk usage...."
+}
+
+check_services() {
+	echo "checking critical services...."
+}
+
+gen_report() {
+	echo "generating HTML report...."
+}
+
+main() {
+	echo "======================================================================================================"
+	echo "	SYSTEM HEALTH MONITOR"
+	echo "	Date:$(date)"
+	echo "======================================================================================================"
+	echo ""
+	
+	check_cpu
+	check_memory
+	check_disk
+	check_services
+	gen_report
+}
+
+
+
+main
 
 
