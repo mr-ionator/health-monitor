429c2f80590473abec42e72e1acfcf29e4fe0487
mr-ionator <siddh05nov@gmail.com>
2026-02-23 17:32:21 +0000

    added logging at function level

--- system_health.sh
+++ system_health.sh
@@ -14,9 +14,17 @@ RED='\e[31m'
 GREEN='\e[32m'
 YELLOW="\[33m"
 NC="\e[0m"
-
+SERVICES=("ssh" "cron" "NetworkManager")
 #functions
 
+
+LOG_DIR="./.logs"
+mkdir -p "$LOG_DIR"
+LOG_FILE="$LOG_DIR/health_$(date +%Y-%m-%d).log"
+
+log_message() {
+	echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1" | tee -a "$LOG_FILE"
+}
 
 check_cpu() {
 	echo "checking cpu usage...."
@@ -35,6 +43,7 @@ check_cpu() {
 	fi
 	echo -e "${color}CPU USAGE: ${cpu_usage}% [${status}]${NC}"
 	echo $cpu_usage > /tmp/health_cpu.tmp
+	log_message "CPU:USAGE ${cpu_usage}% [${status}]"
 }
 
 check_memory() {
@@ -56,10 +65,11 @@ check_memory() {
 	fi
 	echo -e "${color}Memory Usage: ${u_mem_i}MB / ${T_mem_i}MB (${percentage}%) [${status}]${NC}"
 	echo $percentage > /tmp/health_memory.tmp
+	log_message "Memory:USAGE ${u_mem_i}MB [${status}]"
 }
 
 check_disk() {
-	echo "cecking disk usage...."
+	echo "checking disk usage...."
 	df -h | grep -vE 'tmpfs|loop|udev' | tail -n +2 | while read line; do
 		filesystem=$(echo $line | awk '{print $1}')
 		usage=$(echo $line | awk '{print $5}' | sed 's/%//')
@@ -83,6 +93,13 @@ check_disk() {
 
 check_services() {
 	echo "checking critical services...."
+	for service in "${SERVICES[@]}"; do
+		if systemctl is-active --quiet "$service"; then
+			echo -e "${GREEN} $service is running${NC}"
+		else
+			echo -e "${RED} $service is not running${NC}"
+		fi
+ 	done
 }
 
 gen_report() {
